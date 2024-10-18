b94ec9cbf2c49f2ad53086423e64bbb1122a5aa6
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2024-10-18 17:01:47 +0100

    Plotter now has temp x-axis

--- make_pyplot.py
+++ make_pyplot.py
@@ -2,23 +2,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+import datetime
+
 
 readings: pd.DataFrame = pd.read_csv("./sensor_data.csv");
 readings.columns = ["time", "temp", "humidity"];
 # Parse the dateimte, and require an exact format match
-# readings.time = pd.to_datetime(readings.time, format="%Y-%m-%d %H:%M:%S", exact=True).astype(int);
+readings.time = pd.to_datetime(readings.time, format="%Y-%m-%d %H:%M:%S", exact=True);
 
-# print(readings[readings.temp == 21.87]);
+# readings = readings[readings.temp == 21.87];
 sns.set_theme();
 
 plt.figure();
 ax = plt.subplot(2, 1, 1);
 ax.set_title("Temprature");
-sns.lineplot(readings[["time", "temp"]], ax=ax);
+sns.lineplot(readings[["time", "temp"]], x="time", y="temp", ax=ax);
 
 ax = plt.subplot(2, 1, 2);
 ax.set_title("Humidity");
-sns.lineplot(readings[["time", "humidity"]], ax=ax);
+sns.lineplot(readings[["time", "humidity"]], x="time", y="humidity", ax=ax);
 
 
 plt.tight_layout();
